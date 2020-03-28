da79a433df54dc3bc6e1acc47d57949d10913f97
howard <howard.montecillo@yahoo.com>
2020-03-28 14:00:07 +0800

    changed seats to hbs

--- controller/index.js
+++ controller/index.js
@@ -58,6 +58,12 @@ const indexFunctions = {
     getSeats: (req, res, next)=>{
         res.render("seats", {
             pageName: "Reserve Seats",
+            seatRow: [
+                ["1A","1B","1C","1D","1E","1F","1G","1H"],
+                ["2A","2B","2C","2D","2E","2F","2G","2H"],
+                ["3A","3B","3C","3D","3E","3F","3G","3H"],
+                ["4A","4B","4C","4D","4E","4F","4G","4H"]
+            ]
         })
     },
 

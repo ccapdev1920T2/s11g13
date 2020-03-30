387ec9454ca35c9eff6bf8a1e8984f8abc3a85a7
howard <howard.montecillo@yahoo.com>
2020-03-30 21:27:59 +0800

    db functions working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const hbs = require("hbs");
 const bodyParser = require("body-parser");
+const db = require('./database');
 
 const app = express();
 const port = 3000;
@@ -62,3 +63,6 @@ app.listen(port, ()=>{
     console.log(`App listening at port ${port}`);
     console.log(`Access at localhost:${port}/`);
 })
+
+db.createDatabase();
+db.createCollection("myWorld");
--- database.js
+++ database.js
@@ -8,192 +8,196 @@ const options = {useUnifiedTopology: true};
 const databaseName = "TicketLeaveItDB"; //Database Name
 const tableUsers = 'users';
 
-function createDatabase(){
-    client.connect(url, options, (err, db)=>{
-        if(err) throw err;
-        else
-            console.log("Database has been created.");
-        db.close();
-    })
-}
+module.exports = {
 
-function createCollection(collectionName){
-    client.connect(url, options, (err, db)=>{
-        if(err) throw err;
-
-        let database = db.db(databaseName);
-        database.createCollection(collectionName, (err, res)=>{
+    createDatabase: function(){
+        client.connect(url, options, (err, db)=>{
             if(err) throw err;
             else
-                console.log("Collection `" + collectionName + "` created.");
-            db.close();
-        });
-    });
-}
-
-function insertOne(collectionName, record){
-    client.connect(url, options, (err, db)=>{
-        if(err) throw err;
-
-        let database = db.db(databaseName);
-
-        database.collection(collectionName).insertOne(record, (err, res)=>{
-            if (err) throw err;
-            else
-                console.log("1 record has been appended");
-            db.close();
-        });
-
-    });
-}
-
-function insertMany(collectionName, records){
-    client.connect(url, options, (err, db)=>{
-        if(err) throw err;
-
-        let database = db.db(databaseName);
-
-        database.collection(collectionName).insertMany(records, (err, res)=>{
-            if (err) throw err;
-            else
-                console.log("Records appended: " + res.insertedCount);
-            db.close();
-        });
-
-    });
-}
-
-function findOne(collectionName, query){
-    client.connect(url, options, (err, db)=>{
-        if(err) throw err;
-
-        let database = db.db(databaseName);
-
-        database.collection(collectionName).findOne(query, (err, res)=>{
-            if (err) throw err;
-            else
-                console.log(res);
-            db.close();
-        });
-    });
-}
-
-/* 
-Projection - find only specific documents //MYSQL where clause??
-using field: 1 or 0 as flag 
-{_id: 0, name: 1} //Shows only name
-{_id: 0, age: 0} //shows everything except age
-
-Sorting - sort documents //MYSQL orderBy clause
-using flag 1 - Ascending
-flag 0 - descending
-{_id: 0, name: 1} //Sorted in ascending order
-
-Limit //MYSQL Limit cause
-limits results to specific number
-*/
-function findMany(collectionName, query, sortAs = null, limit = null, proj = null){
-    client.connect(url, options, (err, db)=>{
-        if(err) throw err;
-
-        let database = db.db(databaseName);
-
-        database.collection(collectionName).find(query, {projection: proj})
-        .sort(sortAs)
-        .limit(limit)
-        .toArray((err, res)=>{
-            if (err) throw err;
-            else
-                console.table(res);
-            db.close();
-        });
-    });
-}
-
-function deleteOne(collectionName, filter){
-    client.connect(url, options, (err, db)=>{
-        if(err) throw err;
-        
-        let database = db.db(databaseName);
-
-        database.collection(collectionName).deleteOne(filter, (err, res)=>{
-            if (err) throw err;
-            else 
-                console.log("1 Document has been deleted.");
-            db.close();
-        });
-    });
-}
-
-function deleteMany(collectionName, filter){
-    client.connect(url, options, (err, db)=>{
-        if(err) throw err;
-        
-        let database = db.db(databaseName);
-
-        database.collection(collectionName).deleteMany(filter, (err, res)=>{
-            if (err) throw err;
-            else 
-                console.log("Documents deleted: " + res.deletedCount);
-            db.close();
-        });
-    });
-}
-
-function dropCollection(collectionName){
-    client.connect(url, options, (err, db)=>{
-        if(err) throw err;
-        var database = db.db(databaseName);
-        database.collection(collectionName).drop((err, res)=>{
-            if(err) throw err;
-            if(res) 
-                console.log("Collection `" + collectionName + "` dropped.");
+                console.log("Database has been created.");
             db.close();
         })
-    })
-}
+    },
 
-function updateOne(collectionName, filter, newValue){
-    client.connect(url, options, (err, db)=>{
-        if(err) throw err;
-        
-        let database = db.db(databaseName);
+    createCollection: function(collectionName){
+        client.connect(url, options, (err, db)=>{
+            if(err) throw err;
 
-        database.collection(collectionName).updateOne(filter, newValue, (err, res)=>{
-            if (err) throw err;
-            else 
-                console.log("1 Document updated.");
-            db.close();
+            let database = db.db(databaseName);
+            database.createCollection(collectionName, (err, res)=>{
+                if(err) throw err;
+                else
+                    console.log("Collection `" + collectionName + "` created.");
+                db.close();
+            });
         });
-    });
-}
+    },
 
-function updateOne(collectionName, filter, newRecord){
-    client.connect(url, options, (err, db)=>{
-        if(err) throw err;
-        
-        let database = db.db(databaseName);
+    insertOne: function(collectionName, record){
+        client.connect(url, options, (err, db)=>{
+            if(err) throw err;
 
-        database.collection(collectionName).updateOne(filter, newRecord, (err, res)=>{
-            if (err) throw err;
-            else 
-                console.log("1 record updated.");
-            db.close();
+            let database = db.db(databaseName);
+
+            database.collection(collectionName).insertOne(record, (err, res)=>{
+                if (err) throw err;
+                else
+                    console.log("1 record has been appended");
+                db.close();
+            });
+
         });
-    });
-}
+    },
 
-function updateMany(collectionName, filter, newRecords){
-    client.connect(url, options, (err, db)=>{
-        if(err) throw err;
-        
-        let database = db.db(databaseName);
+    insertMany: function(collectionName, records){
+        client.connect(url, options, (err, db)=>{
+            if(err) throw err;
 
-        database.collection(collectionName).updateMany(filter, newRecords, (err, res)=>{
-            if (err) throw err;
-            else 
-                console.log("Documents updated: " + res.modifiedCount);
-            db.close();
+            let database = db.db(databaseName);
+
+            database.collection(collectionName).insertMany(records, (err, res)=>{
+                if (err) throw err;
+                else
+                    console.log("Records appended: " + res.insertedCount);
+                db.close();
+            });
+
         });
-    });
+    },
+
+    findOne: function(collectionName, query){
+        client.connect(url, options, (err, db)=>{
+            if(err) throw err;
+
+            let database = db.db(databaseName);
+
+            database.collection(collectionName).findOne(query, (err, res)=>{
+                if (err) throw err;
+                else
+                    console.log(res);
+                db.close();
+            });
+        });
+    },
+
+    /* 
+    Projection - find only specific documents //MYSQL where clause??
+    using field: 1 or 0 as flag 
+    {_id: 0, name: 1} //Shows only name
+    {_id: 0, age: 0} //shows everything except age
+
+    Sorting - sort documents //MYSQL orderBy clause
+    using flag 1 - Ascending
+    flag 0 - descending
+    {_id: 0, name: 1} //Sorted in ascending order
+
+    Limit //MYSQL Limit cause
+    limits results to specific number
+    */
+    findMany: function(collectionName, query, sortAs = null, limit = null, proj = null){
+        client.connect(url, options, (err, db)=>{
+            if(err) throw err;
+
+            let database = db.db(databaseName);
+
+            database.collection(collectionName).find(query, {projection: proj})
+            .sort(sortAs)
+            .limit(limit)
+            .toArray((err, res)=>{
+                if (err) throw err;
+                else
+                    console.table(res);
+                db.close();
+            });
+        });
+    },
+
+    deleteOne: function(collectionName, filter){
+        client.connect(url, options, (err, db)=>{
+            if(err) throw err;
+            
+            let database = db.db(databaseName);
+
+            database.collection(collectionName).deleteOne(filter, (err, res)=>{
+                if (err) throw err;
+                else 
+                    console.log("1 Document has been deleted.");
+                db.close();
+            });
+        });
+    },
+
+    deleteMany: function(collectionName, filter){
+        client.connect(url, options, (err, db)=>{
+            if(err) throw err;
+            
+            let database = db.db(databaseName);
+
+            database.collection(collectionName).deleteMany(filter, (err, res)=>{
+                if (err) throw err;
+                else 
+                    console.log("Documents deleted: " + res.deletedCount);
+                db.close();
+            });
+        });
+    },
+
+    dropCollection: function(collectionName){
+        client.connect(url, options, (err, db)=>{
+            if(err) throw err;
+            var database = db.db(databaseName);
+            database.collection(collectionName).drop((err, res)=>{
+                if(err) throw err;
+                if(res) 
+                    console.log("Collection `" + collectionName + "` dropped.");
+                db.close();
+            })
+        })
+    },
+
+    updateOne: function(collectionName, filter, newValue){
+        client.connect(url, options, (err, db)=>{
+            if(err) throw err;
+            
+            let database = db.db(databaseName);
+
+            database.collection(collectionName).updateOne(filter, newValue, (err, res)=>{
+                if (err) throw err;
+                else 
+                    console.log("1 Document updated.");
+                db.close();
+            });
+        });
+    },
+
+    updateOne: function(collectionName, filter, newRecord){
+        client.connect(url, options, (err, db)=>{
+            if(err) throw err;
+            
+            let database = db.db(databaseName);
+
+            database.collection(collectionName).updateOne(filter, newRecord, (err, res)=>{
+                if (err) throw err;
+                else 
+                    console.log("1 record updated.");
+                db.close();
+            });
+        });
+    },
+
+    updateMany: function(collectionName, filter, newRecords){
+        client.connect(url, options, (err, db)=>{
+            if(err) throw err;
+            
+            let database = db.db(databaseName);
+
+            database.collection(collectionName).updateMany(filter, newRecords, (err, res)=>{
+                if (err) throw err;
+                else 
+                    console.log("Documents updated: " + res.modifiedCount);
+                db.close();
+            });
+        });
+    }
+
 }

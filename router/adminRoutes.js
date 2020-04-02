35c73920977efe0d467e39798adebcdea53cd066
jhcagaoan <jhcagaoan@gmail.com>
2020-04-03 04:16:17 +0800

    patched routes

--- router/adminRoutes.js
+++ router/adminRoutes.js
@@ -6,5 +6,9 @@ const adminController = require("../controllers/adminController");
 ///this path is to '/admin'
 router.get('/', adminController.getAdminBoard);
 router.post('/',adminController.postAdminBoard);
+//add show
+router.post('/addShow',adminAddShow.postShow);
+//add movie
+router.post('/addMovie',adminAddMovie.postMovie);
 
 module.exports = router;

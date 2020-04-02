17621b4eed86e0c9831e15f5d0bc337b32aecbbb
Bianca Benedictos <bianca_benedictos@dlsu.edu.ph>
2020-04-03 05:02:02 +0800

    added auth checking

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,3 +1,18 @@
+const jwt = require('jsonwetoken');
+
+// path to use this checkAuth:
+// const checkAuth = require('../middleware/heck-auth');
+
 module.exports = (req, res, next) => {
-    
+    //we should failed if we got no token //wow
+    try{
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        req.userData = decoded;
+        next();
+    } catch (error){
+        return res.status(401).json({
+            message: 'Authentication failed'
+        });
+    }
 };

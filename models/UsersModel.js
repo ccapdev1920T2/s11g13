647e79a71f3665f33062104bd64b0c750ebf6815
Bianca Benedictos <bianca_benedictos@dlsu.edu.ph>
2020-04-03 04:58:40 +0800

    edited

--- models/UsersModel.js
+++ models/UsersModel.js
@@ -3,7 +3,10 @@ const mongoose = require ('mongoose');
 const userSchema = mongoose.Schema({
     // unique
     _id: mongoose.Schema.Types.ObjectId,
-    email: {type: String, required: true, unique:true, 
+    email: {
+        type: String, 
+        required: true, 
+        unique:true, 
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
     username: {type: String, required: true, unique:true},

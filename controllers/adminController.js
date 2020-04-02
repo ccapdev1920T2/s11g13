e67ed780692f39e95685194a1f65bff15106a83f
howard <howard.montecillo@yahoo.com>
2020-04-03 00:09:43 +0800

    add show and movie working

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -94,61 +94,6 @@ const adminController = {
             ],
         }
         )*/
-    },
-
-    postAdminBoard: function(req, res, next) {
-        /*
-        var record = {
-            title: req.body.addMovieTitle,
-            genre: req.body.addMovieGenre,
-            aveScore: req.body.addMovieScore,
-            synopsis: req.body.addMovieSynopsis,
-            cast: req.body.addMovieCast
-        }
-        */
-
-        let retrievedData = {
-            genre: req.body.addMovieGenre,
-            title: req.body.addMovieTitle,
-            aveScore: req.body.addMovieScore,
-            synopsis: req.body.addMovieSynopsis,
-            cast: req.body.addMovieCast,
-            posterUrl:null,
-            trailerUrl:null,
-        }
-
-        //db.insertOne(Shows,retrievedData1);
-        db.insertOne(Movies,retrievedData);
-        db.findMany(Movies,{},'title movieID',function(movie){
-                    db.findMany(Shows,{},'movieID date time',function(show){
-                        res.render('admin', {
-                         pageName: "Admin Dashboard",
-                         isSignedIn: true,
-                         username: "Bh0sZxCArr3n",
-                         movies: movie,
-                         show: [{ date: '04-12-20', time: '12:45 - 14:15'}]
-                         /*[
-                             {title: 'MovieComAdventure', genre: 'Comedy, Adventure', rating: '4', day: 1, date: '04-12-20', time: '12:45 - 14:15'},
-                             {title: 'MovieDramaAction', genre: 'Drama, Action', rating: '5', day: 1, date: '04-12-20', time: '12:45 - 14:15'},
-                             {title: 'MovieMysteryAdventure', genre: 'Mystery, Adventure', rating: '3', day: 1, date: '04-12-20', time: '12:45 - 14:15'},
-                             {title: 'MovieComAdventure', genre: 'Comedy, Adventure', rating: '4', day: 2, date: '04-13-20', time: '12:45 - 14:15'},
-                             {title: 'MovieDramaAction', genre: 'Drama, Action', rating: '5', day: 2, date: '04-13-20', time: '12:45 - 14:15'},
-                             {title: 'MovieMysteryAdventure', genre: 'Mystery, Adventure', rating: '3', day: 2, date: '04-13-20', time: '12:45 - 14:15'},
-                             {title: 'MovieComAdventure', genre: 'Comedy, Adventure', rating: '4', day: 3, date: '04-14-20', time: '12:45 - 14:15'},
-                             {title: 'MovieDramaAction', genre: 'Drama, Action', rating: '5', day: 3, date: '04-14-20', time: '12:45 - 14:15'},
-                             {title: 'MovieThrillerPsychological', genre: 'Thriller, Psychological', rating: '4', day: 3, date: '04-14-20', time: '12:45 - 14:15'},
-                             {title: 'MovieComAdventure', genre: 'Comedy, Adventure', rating: '4', day: 4, date: '04-15-20', time: '12:45 - 14:15'},
-                             {title: 'MovieDramaAction', genre: 'Drama, Action', rating: '5', day: 5, date: '04-16-20', time: '12:45 - 14:15'},
-                             {title: 'MovieThrillerPsychological', genre: 'Thriller, Psychological', rating: '4', day: 6, date: '04-17-20', time: '12:45 - 14:15'},
-                             {title: 'MovieDramaAction', genre: 'Drama, Action', rating: '5', day: 6, date: '04-17-20', time: '12:45 - 14:15'},
-                             {title: 'MovieThrillerPsychological', genre: 'Thriller, Psychological', rating: '4', day: 7, date: '04-18-20', time: '12:45 - 14:15'},
-                             {title: 'MovieDramaAction', genre: 'Drama, Action', rating: '5', day: 7, date: '04-18-20', time: '12:45 - 14:15'},
-                         ]*/,
-                        }) 
-                    })
-                           
-                        });
-        
     }
 }
 
--- router/adminRoutes.js
+++ router/adminRoutes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express();
 
 const adminController = require("../controllers/adminController");
+const adminAddMovie = require("../controllers/addMovie.js");
+const adminAddShow = require("../controllers/addShow.js");
 
 ///this path is to '/admin'
 router.get('/', adminController.getAdminBoard);
-router.post('/',adminController.postAdminBoard);
+router.post('/addShow',adminAddShow.postShow);
+router.post('/addMovie',adminAddMovie.postMovie);
 
 module.exports = router;

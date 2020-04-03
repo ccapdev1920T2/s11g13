56953125041e8b9d38d9c905b5b79177bf85cbcf
howard <howard.montecillo@yahoo.com>
2020-04-04 00:06:54 +0800

    can edit but not yet update

--- router/adminRoutes.js
+++ router/adminRoutes.js
@@ -4,12 +4,14 @@ const router = express();
 const adminController = require("../controllers/adminController");
 const adminAddMovie = require("../controllers/addMovie.js");
 const adminAddShow = require("../controllers/addShow.js");
-const adminRemoveShow = require("../controllers/removeShow.js")
+const adminRemoveShow = require("../controllers/removeShow.js");
+const adminChangeShow = require("../controllers/updateShow.js");
 
 ///this path is to '/admin'
 router.get('/', adminController.getAdminBoard);
 router.post('/addShow',adminAddShow.postShow);
 router.post('/addMovie',adminAddMovie.postMovie);
 router.delete('/removeShow',adminRemoveShow.deleteShow);
+router.put('/changeShow',adminChangeShow.updateShow);
 
 module.exports = router;

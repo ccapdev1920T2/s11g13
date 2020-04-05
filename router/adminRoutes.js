94b813004de60dea9534de56a0a961a8ded0b4ed
howard <howard.montecillo@yahoo.com>
2020-04-05 11:03:28 +0800

    changed to correct route of admin

--- router/adminRoutes.js
+++ router/adminRoutes.js
@@ -9,17 +9,9 @@ const adminChangeShow = require("../controllers/updateShow.js");
 
 ///this path is to '/admin'
 router.get('/', adminController.getAdminBoard);
-
-router.post('/', adminController.postAdminBoard);
-//add show
-router.post('/addShow', adminController.postAddShow);
-//add movie
-router.post('/addMovie', adminController.postAddMovie);
-
 router.post('/addShow',adminAddShow.postShow);
 router.post('/addMovie',adminAddMovie.postMovie);
 router.delete('/removeShow',adminRemoveShow.deleteShow);
 router.put('/changeShow',adminChangeShow.updateShow);
-
 
 module.exports = router;

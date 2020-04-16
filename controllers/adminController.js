9be4578789f652c6adab8cc12a9ac3c3bb6f5f20
howard <howard.montecillo@yahoo.com>
2020-04-16 19:32:32 +0800

    added .load function when editing shows

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -149,6 +149,7 @@ const adminController = {
         db.findMany(Movies,{},'title _id',function(movie){
             Shows.find().select("time date dayOfWeek").populate('movieID').exec().then(s=>{
                 let show = [];
+                console.log(s);
                 for (let i=0;i<s.length;i++)
                 {
                     var d = new Date(s[i].date); //ISODate

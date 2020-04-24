a13deabf4f9cdbecab9ce812ff95917521520e9b
jhcagaoan <jhcagaoan@gmail.com>
2020-04-24 20:23:09 +0800

    AJAX for backend validation for payment

--- public/scripts/HBS-payment.js
+++ public/scripts/HBS-payment.js
@@ -1,4 +1,6 @@
 $(document).ready(()=>{
+    $('[data-toggle="tooltip"]').tooltip(); //initialize tooltip for CVV
+
     for(let i = 1; i <= 12; i++){
         let x = document.createElement("option");
         x.setAttribute("value", i);
@@ -14,5 +16,53 @@ $(document).ready(()=>{
         $(x).html(currYear + i);
         $("#expiryYear").append(x);
     }
+
+    $("#cardNum").on("keyup blur", ()=>{
+        let cardNum = $("#cardNum").val();
+        
+        if (cardNum == ""){
+            $("#ccnumfeedback").hide();
+            $("#cardNum").removeClass("is-valid")
+            $("#cardNum").removeClass("is-invalid")
+        }
+        else{
+            $.get('/checkCCNumber', {cardNum: cardNum}, result=>{
+                if (result){
+                    $("#ccnumfeedback").hide();
+                    $("#cardNum").removeClass("is-invalid")
+                    $("#cardNum").addClass("is-valid")
+                }
+                else {
+                    $("#ccnumfeedback").show();
+                    $("#cardNum").removeClass("is-valid")
+                    $("#cardNum").addClass("is-invalid")
+                }
+            })
+        }
+    })
+
+    $("#cardCIV").on("keyup blur", ()=>{
+        let cardCVV = $("#cardCIV").val();
+        
+        if (cardCVV == ""){
+            $("#cvvfeedback").hide();
+            $("#cardCIV").removeClass("is-valid")
+            $("#cardCIV").removeClass("is-invalid")
+        }
+        else{
+            $.get('/checkCVV', {cardCVV: cardCVV}, result=>{
+                if (result){
+                    $("#cvvfeedback").hide();
+                    $("#cardCIV").removeClass("is-invalid")
+                    $("#cardCIV").addClass("is-valid")
+                }
+                else {
+                    $("#cvvfeedback").show();
+                    $("#cardCIV").removeClass("is-valid")
+                    $("#cardCIV").addClass("is-invalid")
+                }
+            })
+        }
+    })
 
 })

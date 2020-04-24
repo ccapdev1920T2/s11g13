f4bc44e1e55ed3944898bcccaef2250169764509
jhcagaoan <jhcagaoan@gmail.com>
2020-04-25 00:00:24 +0800

    Fixed bug not adding to CCInfos db

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -186,22 +186,16 @@ const indexController = {
                 let month = req.body.expiryMonth;
                 let year = req.body.expiryYear;
                 let day;
-                switch(req.body.expiryMonth){
-                    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-                        day = "31";
-                        break;
-                    case 9: case 4: case 6: case 11:
-                        day = "30";
-                        break;
-                    case 2:
-                        //leap year
-                        if (year%4 == 0 && year%100 != 0)
-                            day = "29";
-                        else day = "28";
-                        break;
+                if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+                    day = "31"
+                else if (month == 9 || month == 4 || month == 6 || month == 11)
+                    day = "30";
+                else if (month == 2){
+                    if (year%4 == 0 && year%100 != 0)
+                        day = "29";
+                    else day = "28";
                 }
-                //0 based index for month
-                let expdate = new Date(year, month - 1, day)
+                let expdate = year + "-" + month + "-" + day;
                 let creditCard = new CCInfos({
                     email: u.email,
                     ccnumber: req.body.cardNum,

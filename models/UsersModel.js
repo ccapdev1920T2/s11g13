f2de35115c7b96dfdcd67642055b5b22aaa11520
Bianca Benedictos <bianca_benedictos@dlsu.edu.ph>
2020-04-26 00:53:35 +0800

    cleaned userModel

--- models/UsersModel.js
+++ models/UsersModel.js
@@ -1,7 +1,6 @@
 const mongoose = require ('mongoose');
 
 const userSchema = mongoose.Schema({
-    // unique
     _id: mongoose.Schema.Types.ObjectId,
     token: {type: String, required:false},
     email: {
@@ -11,17 +10,12 @@ const userSchema = mongoose.Schema({
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
     username: {type: String, required: true, unique:true},
-    // pass
     password: {type: String, required: true},
-    // gen info
     userType: {type: String, required: true},
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
-    // optional
     mobileNumber: {type: String, required: false},
     pic: {type: String, required: false},
-    // other stuffzz
-    // saka ko na lagay ksks
 });
 
 module.exports = mongoose.model('users', userSchema);

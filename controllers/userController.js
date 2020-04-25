501f36c750a3af5176a696905c38f375eb798fd8
howard <howard.montecillo@yahoo.com>
2020-04-25 16:22:13 +0800

    update picture (not final)

--- controllers/userController.js
+++ controllers/userController.js
@@ -83,7 +83,7 @@ const userController = {
             lastName: req.body.lName,
             mobileNumber: req.body.Mobile,
             email: req.body.Email,
-            //pic: req.body.pic,
+            pic: "/assets/"+req.body.pic,
         },profile=>{});
 
     },
